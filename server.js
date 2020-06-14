829ff0ca7cad820f909dc3cd86ab3dacb5b6f6f4
Henrique Silva <60949741+heryssrique@users.noreply.github.com>
2020-06-14 17:40:33 -0300

    1.2
    dados bd

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const Pool = require('pg').Pool;
 const cors = require('cors'); 
 
 const pool = new Pool({
-    user:'lrnvydpyoedmbk',
-    password:'4790e37632ed49bad9ab1ddcbe2d1dfd19ff85ee58b7e55791950e33cd44e1a4',
-    host:'ec2-18-214-211-47.compute-1.amazonaws.com',
-    database:'dd4i6bq329eolg',
+    user:'kkvmltqygkvcnf',
+    password:'52680f75a1092e20fd407a7e0b17eddd3e55d04a58b073c7d4ea4ea0d3a8bedc',
+    host:'ec2-35-169-254-43.compute-1.amazonaws.com',
+    database:'d7dkc9ht6gmvpe',
     port: 5432,
     ssl: {rejectUnauthorized: false}
 });
@@ -84,65 +84,6 @@ server.patch('/tarefa/:id/naoentregue', async function(request, response) {
     await pool.query(sql, [id]);
     return response.status(204).send();
 });
-
-
-
-
-
-
-//CRUD EM MEMÓRIA
-
-//const server = express();
-
-//server.use(express.json());
-
-//const tarefas = [
-//  {id: 1, disciplina: 'LP3', date: ('12-05-2020'), entregue: 'true'},
-//  {id: 2, disciplina: 'PO', date: ('15-05-2020'), entregue: 'false'}
-// ]
-
-//server.get('/tarefa', function(request, response) {
-//    response.json(tarefas);
-// })
-
-//server.post('/tarefa', function(request, response) {
-
-//    const {id, disciplina, date, entregue} = request.body;
-
-//    tarefas.push({id, disciplina, date, entregue})
-//    response.status(204).send();
-// })
-
-//server.put('/tarefa/:id', function(request, response){
-//    const id = request.params.id;
-//    const {disciplina, date, entregue} = request.body;
-
-//    for(let i = 0; i < tarefas.length; i++){
-//        if(tarefas[i].id == id) {
-//            tarefas[i].disciplina = disciplina;
-//            tarefas[i].date= date;
-//            tarefas[i].entregue = entregue;
-//            break;
-//        }
-//    }
-
-//    return response.status(204).send();
-// })
-
-//server.delete('/tarefa/:id', function(request, response) {
-
-//    const id = request.params.id;
-
-//    for(let i = 0; i < tarefas.length; i++){
-//        if(tarefas[i].id == id) {
-//            tarefas.splice(i, 1)
-//           break;
-//        }
-//    }
-
-//    return response.status(204).send();
-// })
-
 
 
 
